40a436e5d119350bdae39055a7300dc505b55ded
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2022-05-16 11:07:25 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
-const dotenv = require("dotenv").config
-
 const mongoose = require("mongoose")
 
 
@@ -10,8 +8,9 @@ const mongoose = require("mongoose")
 
 
 const mongo = async function () {
-    console.log(process.env)
-    const atlas = await mongoose.connect(process.env.SCRIPTBD)
+    /*console.log(process.env)*/
+    /*const atlas = await mongoose.connect(process.env.SCRIPTBD)*/
+    const atlas = await mongoose.connect("mongodb+srv://root:Santinho111@habdig7oficial-cluster.ccizs.mongodb.net/?retryWrites=true&w=majority")
 }
 
 
